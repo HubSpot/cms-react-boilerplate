a8d0065ca316263f1d142437d6ca5cdec0ed1492
Kevin Leonard <kleonard@hubspot.com>
2020-05-18 15:28:39 -0400

    sprocket origin tweak per @anthmatic's suggestion

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App({ moduleData }) {
       <header>
         <div className="logo-container">
           <img src={logo} alt="react logo" />
-          <img src={sprocket} alt="sprocket logo" />
+          <img className="sprocket-logo" src={sprocket} alt="sprocket logo" />
         </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
