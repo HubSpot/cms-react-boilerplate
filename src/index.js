46be94fb82b405ea9fbed017195093429ce3844c
Kevin Leonard <kleonard@hubspot.com>
2020-05-18 13:06:39 -0400

    handle instances of the same module

--- src/index.js
+++ src/index.js
@@ -6,15 +6,19 @@ import './index.scss';
 import App from './App';
 import ErrorBoundary from './components/ErrorBoundary';
 
-const __MODULE_DATA__ = JSON.parse(
-  document.querySelector('[type="application/json"]').textContent,
-);
-const root = document.getElementById('cms-react-boilerplate__App');
-const portalId = Number(root.dataset.portalId);
-
-ReactDOM.render(
-  <ErrorBoundary>
-    <App portalId={portalId} moduleData={__MODULE_DATA__} />
-  </ErrorBoundary>,
-  root,
-);
+const targetModules = document.querySelectorAll('[type="application/json"]');
+targetModules.forEach(({ dataset, textContent }) => {
+  const root = document.getElementById(
+    `cms-react-boilerplate__App--${dataset.moduleInstance}`,
+  );
+  return ReactDOM.render(
+    <ErrorBoundary>
+      <App
+        portalId={dataset.portalId}
+        moduleData={JSON.parse(textContent)}
+        moduleInstance={dataset.moduleInstance}
+      />
+    </ErrorBoundary>,
+    root,
+  );
+});
