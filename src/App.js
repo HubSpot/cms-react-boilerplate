2b0bcdfbb9b8af3e60672221b022598c2cedd871
Kevin Leonard <kleonard@hubspot.com>
2020-05-21 15:26:28 -0400

    use individual Logo component instead of container component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './App.scss';
-import LogoContainer from './components/LogoContainer';
+import SpinningLogo from './components/SpinningLogo';
 import Card from './components/Card';
+import reactLogo from './images/logo.svg';
+import sprocketLogo from './images/sprocket.svg';
 
 function App({ moduleData }) {
   // eslint-disable-next-line no-console
@@ -12,7 +14,14 @@ function App({ moduleData }) {
   return (
     <div className="cms-react-boilerplate__container">
       <header>
-        <LogoContainer />
+        <div className="spinning-logo__container">
+          <SpinningLogo src={reactLogo} alt="react logo" />
+          <SpinningLogo
+            src={sprocketLogo}
+            alt="sprocket logo"
+            isSprocket={true}
+          />
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
